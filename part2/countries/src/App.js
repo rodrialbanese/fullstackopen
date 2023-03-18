bef57104373943de0c9a9d4d28ae2dbb56307ad3
Rodrigo Albanese <rodrialbanese@gmail.com>
2023-03-18 14:59:22 -0300

    refactored countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,11 +1,10 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Form from "./components/Form"
 import List from "./components/List"
 import Country from "./components/Country"
-
-const API_WEATHER = process.env.REACT_APP_WEATHER_API
-console.log(API_WEATHER)
+import Weather from './components/Weather'
+import serviceCountries from './services/countries'
+import serviceWeather from './services/weather'
 
 function App() {
   const [countries, setCountries] = useState([])
@@ -16,16 +15,21 @@ function App() {
   
 
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all")
-    .then((response) => {setCountries(response.data)
-    console.log(response.data)})
+    serviceCountries
+    .getAll()
+    .then(response => setCountries(response))
   }, [])
 
   useEffect(() => {
-      if (selectedCountry) {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${selectedCountry.capitalInfo.latlng[0]}&lon=${selectedCountry.capitalInfo.latlng[1]}&appid=${API_WEATHER}&units=metric`)
-        .then((response) => setWeather(response.data))
-        .catch((error) => console.log(error))
+      if (selectedCountry && Object.keys(selectedCountry.capitalInfo).length>0) { // check if capitalInfo is in data (United States Minor Outlying Islands doesnt have capitalInfo)
+        const lat = selectedCountry.capitalInfo.latlng[0]
+        const lng = selectedCountry.capitalInfo.latlng[1]
+        serviceWeather
+        .getWeather(lat, lng)
+        .then((response) => setWeather(response))
+      }
+      else {
+        setWeather(null)
       }
     }, [selectedCountry])
 
@@ -36,16 +40,22 @@ function App() {
     if (_countries.length === 1) {
       setSelectedCountry(_countries[0])
     }
+    else {
+      setSelectedCountry(null)
+    }
   };
 
-  const handleButton = (selected) =>
-    setSelectedCountry(selected) 
+  const handleButton = (selected) => {
+    console.log("selected", selected)
+    setSelectedCountry(selected)}
 
   return (
     <div>
       <Form onChange={handleCountryChange} value={country}/>
       <List countriesList={countriesList} handleButton={handleButton}/>
-      <Country country={selectedCountry} weather={weather}/>
+      <Country country={selectedCountry}/>
+      <Weather country={selectedCountry} weather={weather}/>
+
     </div>
 
   );
