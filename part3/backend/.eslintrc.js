56a68e111800f051af2e127f56aa4af01b9e24d9
Rodrigo Albanese <rodrialbanese@gmail.com>
2023-03-27 09:52:19 -0300

    comenzando testing

---
+++ part3/backend/.eslintrc.js
@@ -0,0 +1,33 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es2021": true,
+		"jest": true,
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"never"
+		]
+	}
+}
